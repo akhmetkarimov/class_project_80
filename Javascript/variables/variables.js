eb3f396b66d895831631f9d090e4ce53ca57f4ab
Yedil Akhmetkarimov <edil.eakhmetkarimov@gmail.com>
2020-09-18 18:50:37 +0600

    asd

---
+++ Javascript/variables/variables.js
@@ -0,0 +1,48 @@
+// alert("welcome to my website")
+// console.log("welcome to my website")
+// document.write("welcome to my website")
+
+// var/let/const
+
+var a = 'hello'
+a = 333 + 223
+a = a + 231
+
+let b = 54.78
+b = 555
+
+const c = 30
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(a + b)
+console.log("welcome " + "yedil")
+
+
+let num1 = 10
+let num2 = 20
+
+
+console.log("num 1", num1)
+console.log("num 2", num2)
+
+
+let num3 = num1 // 7
+num1 = num2 // 2
+num2 = num3 // 7
+
+console.log("num 1", num1)
+console.log("num 2", num2)
+
+
+
+
+
+console.log(num1 + num2) // 9
+console.log(num1 - num2) // 5
+console.log(num1 / num2) // 3.5
+console.log(num1 * num2) // 14
+console.log(num1 % num2) // 1
+
+console.log(8 % 2)
